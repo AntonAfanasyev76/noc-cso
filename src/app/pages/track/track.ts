f701290cfd79b64ad87904451fccd7804ae2fddb
AntonAfanasyev home <anton-afanasyev@mail.ru>
2026-07-05 19:57:23 +0300

    first commit

---
+++ src/app/pages/track/track.ts
@@ -0,0 +1,48 @@
+import { Component, inject, signal } from '@angular/core';
+import { Header } from '../../header/header';
+import { FormsModule } from '@angular/forms';
+import { DeliveryApi } from '../../services/delivery-api';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-track',
+  imports: [Header, FormsModule],
+  templateUrl: './track.html',
+  styleUrl: './track.css',
+})
+export class Track {
+  trackNumber = '';
+  trackResult: any = signal(null);
+
+  toastr = inject(ToastrService);
+
+  constructor(private deliveryApi: DeliveryApi) {}
+
+
+  trackShipment(): void {
+    const rawValue = this.trackNumber.trim();
+
+    if (!rawValue) {
+      this.toastr.error('Заполните номер отправления');
+      return;
+    }
+
+    this.trackResult.set(null);
+    const numericValue = Number(rawValue);
+    if (Number.isNaN(numericValue) || numericValue <= 0) {
+      this.toastr.error('Введите корректный номер отправления');
+      return;
+    }
+
+    this.deliveryApi.getDeliveryInfo(numericValue).subscribe((response) => {
+      if ('error' in response) {
+        this.toastr.error(response.error);
+        return;
+      }
+
+      this.toastr.success("Информация успешно получена!");
+      this.trackResult.set(response);
+    });
+
+  }
+}
